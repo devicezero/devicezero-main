2283e3561229d543a6ad69b4c312b821cd5465ca
Jonas Kröger <jk@devicezero.me>
2018-04-04 17:28:18 +0200

    fix mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,42 @@
  Author     : Atta-Ur-Rehman Shah (http://attacomsian.com)
  */
 $(function () {
-    //init
+  //init
+  init();
+  //init wow effects
+  new WOW().init();
+
+  //scroll menu
+  $(window).scroll(function () {
     init();
-    //init wow effects
-    new WOW().init();
+  });
 
-    //scroll menu
-    $(window).scroll(function () {
-        init();
-    });
+  //page scroll
+  $('a.page-scroll').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 50
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 
-    //page scroll
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 50
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  //init function
+  // function init() {
+  //   var secondFeature = $('#services').offset().top;
+  //   var scroll = $(window).scrollTop();
+  //   if (scroll >= 150) {
+  //     $('.sticky-navigation').css({'background-color': '#88c425'});
+  //   } else {
+  //     $('.sticky-navigation').css({'background-color': 'transparent'});
+  //   }
+  //   if (scroll >= secondFeature - 200) {
+  //     $('.mobileScreen').css({'background-position': 'center top'});
+  //   }
+  //   return false;
+  // }
 
-    //init function
-    function init() {
-        var secondFeature = $('#services').offset().top;
-        var scroll = $(window).scrollTop();
-        if (scroll >= 150) {
-            $('.sticky-navigation').css({"background-color": '#88c425'});
-        } else {
-            $('.sticky-navigation').css({"background-color": 'transparent'});
-        }
-        if (scroll >= secondFeature - 200) {
-            $(".mobileScreen").css({'background-position': 'center top'});
-        }
-        return false;
-    }
-
-    $('#imprint-link').bind('click', function (event) {
-      $('#imprint-text').css({"display": 'block'});
-      event.preventDefault();
-    });
+  $('#imprint-link').bind('click', function (event) {
+    $('#imprint-text').css({'display': 'block'});
+    event.preventDefault();
+  });
 });
